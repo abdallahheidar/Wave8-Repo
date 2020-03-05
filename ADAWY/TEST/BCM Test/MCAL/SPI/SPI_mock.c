508e7c62fc2de0188665f725ef902f502faefb50
3dawy <60049738+3dawy@users.noreply.github.com>
2020-03-06 00:11:11 +0200

    uni test

---
+++ ADAWY/TEST/BCM Test/MCAL/SPI/SPI_mock.c
@@ -0,0 +1,23 @@
+/*
+ * SPI_.c
+ *
+ *  Created on: Mar 5, 2020
+ *      Author: Khaled
+ */
+
+#include "SPI__.h"
+
+void _SPIInitMaster(SPI_clk clock ,SPI_mode phasePolarityMode,SPI_DataOrder dataorder, SPI_Interrupt_t InterruptMode)
+{
+	;/*do nothing*/
+}
+void _SPIInitSlave(SPI_clk clock ,SPI_mode phasePolarityMode,SPI_DataOrder dataorder ,SPI_Interrupt_t InterruptMode ,void (*SPI_Cbk)(uint8_t))
+{
+	;/*do nothing*/
+}
+
+
+void _SPITrancevier(uint8_t data,uint8_t *ptrData)
+{
+	;/*do nothing*/
+}
