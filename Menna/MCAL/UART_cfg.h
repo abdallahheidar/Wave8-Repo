eaaa3e5b7c3756f8c8e72a5767f9c05a9fc51196
MennaEhab <mennaehab21.5@gmail.com>
2020-03-05 19:10:52 +0200

    prev. projects

---
+++ Menna/MCAL/UART_cfg.h
@@ -0,0 +1,21 @@
+/*
+ * UART_cfg.h
+ *
+ * Created: 2/8/2020 5:44:45 PM
+ *  Author: Khaled Magdy
+ */ 
+
+
+#ifndef UART_CFG_H_
+#define UART_CFG_H_
+
+#include "UART.h"
+
+#define TX_GPIO	GPIOD
+#define TX_BIT	BIT1
+
+#define RX_GPIO	GPIOD
+#define RX_BIT	BIT0
+
+
+#endif /* UART_CFG_H_ */
