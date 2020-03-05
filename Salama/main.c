89c85fdc3240a06091ee6c3b20a36c5940f484b3
ahmedsalama09 <60046276+ahmedsalama09@users.noreply.github.com>
2020-03-05 17:51:31 +0200

    Update main.c

--- Salama/main.c
+++ Salama/main.c
@@ -1,42 +1,125 @@
 /*
- * SEND.c
- *
+ * Basic_Comm_Module.c
  *
  * Created: 2020-03-01 9:49:13 PM
- * Author : Ahmed_Sakama
+ * Author : AHMED_SALAMA
  */ 
 
+/************************************************************************/
+/*							INCLUDES                                    */
+/************************************************************************/
 #include "bcm.h"
-#include "registers.h"
 #include "interrupt.h"
 #include "uart.h"
-#include "uart_config.h"
-//extern ptr_Function gptr_BCM_TxC_CBK;
 
-void fun1(){
+/************************************************************************/
+/*							DEFINES                                     */
+/************************************************************************/
+#define MAX_BUFFER_SIZE (30)
+
+
+/*please change it to BCM_SEND if you want to use sending side*/
+#define BCM_SEND
+
+#ifdef BCM_SEND
+/**First half of the project
+	*Description : sending from pc to uart then from spi->spi
+	**receiving data till '@' is received then we send that buffer to the other spi side
+	*please define it to use it*/
 	
-}
-char arr[20] ;
+/************************************************************************/
+/*						DEFINES                                         */
+/************************************************************************/
+#define BCM_TERMINATION		('@')
+	
+/*BCM Buffer creation*/
+uint8_t arr_BCM_Buffer[MAX_BUFFER_SIZE];
+
 int main(void)
 {
+	uint8_t uart_counter = ZERO;
 	
+	/*init uart first and you can modify structure at uart_config.c file*/
+	uart_init(&st_uart_init);
 	
-	BCM_Cfg.Transmit_Cbk = fun1;
-	
+	/*saving everything received by uart to the buffer
+	Terminates when receiving @*/
+	while(1)
+	{
+		/*save data byte by byte in buffer*/
+		arr_BCM_Buffer[uart_counter] = uart_rx();
+		
+		/*terminate when @ is received*/
+		if(arr_BCM_Buffer[uart_counter] == BCM_TERMINATION)
+		{
+				break;
+		}
+		
+		/*increment counter by one*/
+		uart_counter++;
+	}
+
+	/*init the BCM module*/
 	BCM_Init(&BCM_Cfg);
+
+	/*enable global interrupt*/
 	sei();
 	
-	
-
-	BCM_SetupReceive(arr,20);
+	/*send buffer by BCM*/
+	BCM_Send(arr_BCM_Buffer , uart_counter);
 
     while (1) 
     {
-		BCM_RxDispatcher();
+		/*start bcm_dispatch*/
+		BCM_TxDispatcher();
+		
     }
 }
 
-void uart_receive_call()
-{
+/*Other side of project
+*Description : here is the receiving spi side then sending data back to pc
+**the data is sent at BCM after receiving it so please check it
+***please make sure that spi is configure as a slave
+
+*/
+
+#else 
+
+/*create receiving buffer*/
+uint8_t arr_BCM_Buffer[MAX_BUFFER_SIZE];
+
+/*receive function define*/
+void BCM_Receive_Notification(uint16_t uart_size){
 	
+	/*first init uart*/
+	uart_init(&st_uart_init);
+	
+	uint16_t u16_loop = ZERO;
+	
+	/*loop to send all bytes*/
+	for(u16_loop = 0; u16_loop <= uart_size ; u16_loop++)
+	{
+		/*send data back to pc*/
+		uart_tx_send(arr_BCM_Buffer[u16_loop]);
+	}
 }
+
+
+
+int main(void)
+{
+	BCM_Cfg.Recieve_Cbk = BCM_Receive_Notification;
+	
+	/*init BCM*/
+	BCM_Init(&BCM_Cfg);
+
+	sei();
+	/*setup you buffer settings*/
+	BCM_SetupReceive(arr_BCM_Buffer,MAX_BUFFER_SIZE);
+
+	while (1)
+	{
+		BCM_RxDispatcher();
+	}
+}
+#endif
