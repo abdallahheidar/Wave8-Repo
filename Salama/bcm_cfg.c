62b987e0c87251f7294e32e1b9d59efff6f7b7cb
ahmedsalama09 <60046276+ahmedsalama09@users.noreply.github.com>
2020-03-05 17:42:59 +0200

    Add files via upload

---
+++ Salama/bcm_cfg.c
@@ -0,0 +1,13 @@
+/*
+ * bcm_cfg.c
+ *
+ * Created: 2020-03-02 1:31:20 PM
+ *  Author: EbrahimOseif
+ */ 
+#include "bcm_cfg.h"
+
+st_BCM_Cfg_t BCM_Cfg = {
+	SPI_PROTOCOL,
+	NULL,
+	NULL
+};
--- Salama/main.c
+++ Salama/main.c
@@ -1,1 +1,42 @@
+/*
+ * SEND.c
+ *
+ *
+ * Created: 2020-03-01 9:49:13 PM
+ * Author : EbrahimOseif
+ */ 
 
+#include "bcm.h"
+#include "registers.h"
+#include "interrupt.h"
+#include "uart.h"
+#include "uart_config.h"
+//extern ptr_Function gptr_BCM_TxC_CBK;
+
+void fun1(){
+	
+}
+char arr[20] ;
+int main(void)
+{
+	
+	
+	BCM_Cfg.Transmit_Cbk = fun1;
+	
+	BCM_Init(&BCM_Cfg);
+	sei();
+	
+	
+
+	BCM_SetupReceive(arr,20);
+
+    while (1) 
+    {
+		BCM_RxDispatcher();
+    }
+}
+
+void uart_receive_call()
+{
+	
+}
