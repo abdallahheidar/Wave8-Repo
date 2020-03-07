bbe1a62470b7149c751ab02b20b56535b700746f
ayaayman12 <39214939+ayaayman12@users.noreply.github.com>
2020-03-07 02:47:46 +0200

    ..

--- Aya/BCM Unit Testing/BCM_Unit_Testing/SL/BCM/BCM_CFG.c
+++
@@ -1,6 +0,0 @@
-/*
- * BCM_CFG.c
- *
- * Created: 3/2/2020 1:18:33 AM
- *  Author: A_ayman
- */ 
--- Aya/BCM Unit Testing/BCM_Unit_Testing/Testing/BCM_unit_Testing.h
+++
@@ -1,7 +0,0 @@
-#ifndef BCM_unit_Testing_H_
-#define BCM_unit_Testing_H_
-#include "../SL/Utilis/std_types.h"
-#include "../SL/Utilis/module_retval.h"
-extern uint8_t gu8_UART_Init_Return;
-extern uint8_t gu8_SPI_Init_Return;
-#endif
