38c92a763bd3d98a961dd39e837fb535d968615e
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2020-03-11 09:42:26 +0200

    adding-SOS

---
+++ Abdelbary/ServiceLayer/SOS/SOS_pbcfg.h
@@ -0,0 +1,19 @@
+/*
+ * SOS_pbcfg.h
+ *
+ * Created: 2/24/2020 11:51:43 AM
+ *  Author: mahmo
+ */ 
+
+
+#ifndef SOS_PBCFG_H_
+#define SOS_PBCFG_H_
+
+#include "SOS_pbcfg.h"
+
+
+
+
+
+
+#endif /* SOS_PBCFG_H_ */
