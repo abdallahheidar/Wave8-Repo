9dc2cf92f017a37e34256389b37b92c4c47eca1f
Ramadan92 <60047313+Ramadan92@users.noreply.github.com>
2020-03-11 15:17:53 +0200

    Add files via upload

---
+++ Ehab/SOS_Config.c
@@ -0,0 +1,17 @@
+/*
+ * TMU_Config.c
+ *
+ * Created: 2/25/2020 11:25:46 AM
+ *  Author: Ehab & Reem
+ */ 
+
+#include "TMU_Config.h"
+
+TMU_ConfigType TMU_Config = 
+{
+	/*For timer channel 0 write TIMER_CH0
+	For timer channel 2 write TIMER_CH2
+	Note: timer channel 1 is not valid */
+	TIMER_CH0
+	
+};
