27a47d09a011e342d937b0bd1ec879555bc6a8b8
ahmedsalama09 <60046276+ahmedsalama09@users.noreply.github.com>
2020-03-11 15:26:51 +0200

    salama sos unit test

---
+++ Slama/SOS_TS/SOS_Config.h
@@ -0,0 +1,20 @@
+/*
+ * SOS_Config.h
+ *
+ * Created: 25-Feb-20 5:24:59 PM
+ *  Author: ahmed
+ */ 
+
+
+#ifndef SOS_CONFIG_H_
+#define SOS_CONFIG_H_
+
+/************************************************************************/
+/*		NOTES 1- This file is for inserting buffer size
+			  2- for errors please check Error_table head filer         */
+/************************************************************************/
+
+#define SOS_BUFFER		(10)
+
+
+#endif /* SOS_CONFIG_H_ */
