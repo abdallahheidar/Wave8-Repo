d0c0c95bccdc2bd86994323d1008df3f5241f8e2
muhammed13 <40000647+muhammed13@users.noreply.github.com>
2020-03-25 05:51:28 +0200

    Car_StaticDesign

---
+++ Gamal/Static_DesignChallenge/Debug/MCAL/Timer.d
@@ -0,0 +1,13 @@
+MCAL/Timer.o MCAL/Timer.o: ../MCAL/Timer.c ../MCAL/Timer.h \
+  ../MCAL/registers.h ../MCAL/../std_types.h ../MCAL/../common_macros.h \
+  ../MCAL/interrupt.h
+
+../MCAL/Timer.h:
+
+../MCAL/registers.h:
+
+../MCAL/../std_types.h:
+
+../MCAL/../common_macros.h:
+
+../MCAL/interrupt.h:
