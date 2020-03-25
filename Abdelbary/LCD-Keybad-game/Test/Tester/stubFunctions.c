4e00136cd9edb7f7b581724504f0f780fa7acb2f
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2020-03-25 02:08:43 +0200

    add SPI_UniteTeste

--- Abdelbary/LCD-Keybad-game/Test/Tester/stubFunctions.c
+++ Abdelbary/LCD-Keybad-game/Test/Tester/stubFunctions.c
@@ -4,6 +4,9 @@
  * Created: 3/13/2020 12:08:44 PM
  *  Author: mahmo
  */ 
+
+#ifdef GCC
+
 #include "stubFunction.h"
 
 uint8_t gu8_Timer_Init_fun_status;
@@ -24,4 +27,15 @@ ERROR_STATUS	Timer_DeInit(uint8_t timer_Ch)
 	return gu8_Timer_DeInit_status;
 }
 
+/*
+`SPI_getData'
+`SPI_sendData'
+`Uart_Init'
+`SPI_init'
+`spi_enable'
+`SPI_init'
+`spi_enable'
+`SPI_sendData'
+`SPI_sendData'*/
 
+#endif
