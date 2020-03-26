8bd7f216432bbeadb1951caa80bedf99ccb057e4
Youssef Harby <59998480+7arby95@users.noreply.github.com>
2020-03-26 21:09:16 +0200

    Add files via upload

---
+++ Harby/Testers/Steering/ECUAL/motor.c
@@ -0,0 +1,63 @@
+/*
+ * motor.c
+ *
+ *  Created on: Feb 18, 2020
+ *      Author: Youssef Harby
+ */
+
+
+/*- INCLUDES -----------------------------------------------*/
+
+#include "motor.h"
+
+
+/*- DEFINES ------------------------------------------------*/
+
+
+
+/*- GLOBAL STATIC VARIABLES --------------------------------*/
+
+
+
+/*- APIs IMPLEMENTATION ------------------------------------*/
+
+ERROR_STATUS Motor_Init(uint8_t Motor_Number)
+{
+
+	return E_OK;
+}
+
+ERROR_STATUS Motor_Direction(uint8_t Motor_Number, uint8_t Motor_Direction)
+{
+
+
+	return E_OK;
+}
+
+ERROR_STATUS Motor_Start(uint8_t Motor_Number, uint8_t Mot_Speed)
+{
+
+
+	return E_OK;
+}
+
+ERROR_STATUS Motor_SpeedUpdate(uint8_t Motor_Number, uint8_t Speed)
+{
+
+
+	return E_OK;
+}
+
+ERROR_STATUS Motor_Stop(uint8_t Motor_Number)
+{
+
+
+	return E_OK;
+}
+
+ERROR_STATUS Motor_GetStatus(uint8_t Motor_Number, uint8_t* Mot_status)
+{
+
+
+	return E_OK;
+}
