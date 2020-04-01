1135f045d79b352b800668d9fd3fa65926b5c9c4
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2020-04-01 21:44:42 +0200

    addTest ouput

--- Abdelbary/SumUniteTesting/sumTest.c
+++ Abdelbary/SumUniteTesting/sumTest.c
@@ -1,26 +1,30 @@
 
 #include <stdio.h>
-
-
+#define E_OK 	0
+#define E_NOK	-1
+#define MAX_INIT	100
+#define ERR_STATUS	int
 ERR_STATUS sum(int a , int b,int* sum)
 {
 ERR_STATUS u8_funStatus = E_OK;
 
-if(MAX_INIT-b > a)
+if(MAX_INIT-b < a)
 u8_funStatus = E_NOK;
 else
 {
 *sum = a+b;
 }
+return u8_funStatus;
 }
 
 int main(void)
 {
 int a[][3] = {{5,5,E_OK},{0,0,E_OK},{MAX_INIT,MAX_INIT-5,E_NOK}};
-int sum = 0;
-for(int i -=0 ; i < 3 ;i++)
+int s = 0;
+for(int i =0 ; i < 3 ;i++)
 {
-int expRet = sum(a[i][0],a[i][1],&sum);
+int expRet = sum(a[i][0],a[i][1],&s);
+printf("testCase %d : expected_output %d , actual_output %d  ",i,a[i][2],expRet);
 if(expRet == a[i][2])
 printf("PASSED\n");
 else
