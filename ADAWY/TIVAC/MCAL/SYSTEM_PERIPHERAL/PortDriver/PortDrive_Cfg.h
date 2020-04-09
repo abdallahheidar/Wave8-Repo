7133e09bde15f272d5e51865ea26401deeb88222
3dawy <60049738+3dawy@users.noreply.github.com>
2020-04-09 23:52:35 +0200

    Add files via upload

---
+++ ADAWY/TIVAC/MCAL/SYSTEM_PERIPHERAL/PortDriver/PortDrive_Cfg.h
@@ -0,0 +1,16 @@
+/*
+ * PortDrive_Cfg.h
+ *
+ * Created: 4/8/2020 7:59:05 PM
+ *  Author: Khaled
+ */ 
+
+
+#ifndef PORTDRIVE_CFG_H_
+#define PORTDRIVE_CFG_H_
+
+
+
+
+
+#endif /* PORTDRIVE_CFG_H_ */
---
+++ ADAWY/TIVAC/MCAL/SYSTEM_PERIPHERAL/PortDriver/PortDriver.h
@@ -0,0 +1,21 @@
+/*
+ * PortDriver.h
+ *
+ * Created: 4/8/2020 6:29:08 PM
+ *  Author: Khaled
+ */ 
+
+
+#ifndef PortDriver_H_
+#define PortDriver_H_
+
+#include "PortDriver_Types.h"
+#include "PortDriver_Lcfg.h"
+#include "PortDrive_Cfg.h"
+
+
+extern void PortDriver_init (void);
+
+
+
+#endif /* PortDriver_H_ */
