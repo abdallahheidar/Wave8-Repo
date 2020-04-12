1fce1e3b672776f3d037f2dee5a5cc40cacb61e0
ayaayman12 <39214939+ayaayman12@users.noreply.github.com>
2020-04-12 07:29:26 +0200

    Common Drivers

---
+++ Aya/System_Clock_Driver_Files/Common_Macros.h
@@ -0,0 +1,16 @@
+/*
+ * Common_Macros.h
+ *
+ * Created: 4/3/2020 2:31:04 PM
+ *  Author: A_ayman
+ */ 
+
+
+#ifndef COMMON_MACROS_H_
+#define COMMON_MACROS_H_
+
+#define SET_BIT(Reg,bit) Reg|=(1<<bit)
+
+
+
+#endif /* COMMON_MACROS_H_ */
---
+++ Aya/System_Clock_Driver_Files/main.c
@@ -0,0 +1,24 @@
+/*
+ * System_Clock_files.c
+ *
+ * Created: 4/6/2020 4:53:07 PM
+ * Author : A_ayman
+ */ 
+
+/*
+*** Inclusions
+*/
+#include "SysCtr.h"
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+
+    SysCtr_init();
+    while (1) 
+    {
+    }
+}
+
