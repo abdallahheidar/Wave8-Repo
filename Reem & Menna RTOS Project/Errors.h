ebafb963fac19d2fe56be85b84383dc583f836b2
Reem-Elareeny <60077840+Reem-Elareeny@users.noreply.github.com>
2020-04-13 19:06:56 +0200

    Add files via upload

---
+++ Reem & Menna RTOS Project/Errors.h
@@ -0,0 +1,34 @@
+/*
+ * Errors.h
+ *
+ * Created: 2/25/2020 10:40:12 AM
+ *  Author: Ehab & Reem
+ */ 
+
+
+#ifndef ERRORS_H_
+#define ERRORS_H_
+
+#define ERROR_NULL_POINTER						1
+#define ERROR_INVALID_PARAMETER				2		   
+#define ERROR_MODULE_NOT_INITIALIZED		3					   
+#define ERROR_MODULE_ALREADY_INITIALIZED	4						   
+#define ERROR_STOP_WITHOUT_START				5				   
+#define ERROR_MULTIPLE_START					6			   
+#define ERROR_FULL_BUFFER						7
+#define ERROR_FALSE_DATA_RECEIVED			8		   
+
+
+#define ERROR_INVALID_PORT						10
+#define ERROR_INVALID_DIR						11
+#define ERROR_INVALID_VALUE					12
+
+#define ERROR_UNAVAILABLE_DISPLAYER			13
+
+
+#define ERROR_DIO_MODULE			100
+#define ERROR_LCD_MODULE			200
+#define ERROR_BTN_MODULE			300	
+
+							   
+#endif /* ERRORS_H_ */
