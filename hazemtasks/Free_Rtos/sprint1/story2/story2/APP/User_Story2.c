3bece097ffc1109721ed90aad584571acce57f43
muhammed13 <40000647+muhammed13@users.noreply.github.com>
2020-04-16 13:17:15 +0200

    Update User_Story2.c

--- hazemtasks/Free_Rtos/sprint1/story2/story2/APP/User_Story2.c
+++ hazemtasks/Free_Rtos/sprint1/story2/story2/APP/User_Story2.c
@@ -28,6 +28,7 @@
 #define ENTER                 0
 #define ASCII_ADDITION        48
 #define ENTRIES_FOR_2_SEC     20
+#define ENTRIES_FOR_DEBOUNCE  2
 /***************************************************************************/
 
 
@@ -55,7 +56,6 @@ void UserStoryTwoInit(void)
 	{
 		xHandle=xTaskGetCurrentTaskHandle();
 		vTaskDelete(xHandle);
-		
 	}
 }
 
@@ -72,10 +72,11 @@ void UserStoryTwoInit(void)
 void UserStoryTwo(void)
 {
 	TickType_t UserStoryOne_LastWakeTime;
-	const TickType_t UserStoryOne_Frequency = ONE_HANDRED_MS; /* to avoid tacking the same input from the user */
+	const TickType_t UserStoryOne_Frequency = ONE_HANDRED_MS;                    /* to avoid tacking the same input from the user */
 	uint8_t au8_Key;
 	uint8_t au8_temp_key;
 	static uint8_t u8_entry_counter=CLEAR;
+	static uint8_t u8_debouncing_counter=CLEAR;
 	static uint8_t u8_waiting_flag=FLAG_LOW;
 	static uint8_t u8_pass_match_flag=CLEAR;
 	static uint8_t u8_counter=CLEAR;
@@ -101,55 +102,66 @@ void UserStoryTwo(void)
 
 			if(au8_Key != NO_KEY_PRESSED)
 			{
-				/* switch on the key value and take the appropriate action */
-				au8_temp_key = au8_Key+ASCII_ADDITION;
-				LCD_displayCharacter(au8_temp_key);
+				/* Increment the debouncing counter */
+				u8_debouncing_counter++;
 
-				/*erase button is pressed */
-				if(au8_Key == '#')
+				/* to prevent any key duplication since the human press is around 100ms */
+				if(u8_debouncing_counter==ENTRIES_FOR_DEBOUNCE)
 				{
-					/* clear the LCD */
-					LCD_clearScreen();
-				}
+					/* clear the debouncing counter */
+					u8_debouncing_counter=CLEAR;
 
-				  /* enter button is pressed */
-				else if(au8_Key == ENTER)
-				{
-					LCD_clearScreen();
-					/* check if 6 digites or more are pressed */
-					if(u8_counter >= SIX)
+					/* switch on the key value and take the appropriate action */
+					au8_temp_key = au8_Key+ASCII_ADDITION;
+					LCD_displayCharacter(au8_temp_key);
+
+					/*erase button is pressed */
+					if(au8_Key == '#')
 					{
-						if(u8_pass_match_flag == MATCHED)
+						/* clear the LCD */
+						LCD_clearScreen();
+					}
+
+					  /* enter button is pressed */
+					else if(au8_Key == ENTER)
+					{
+						LCD_clearScreen();
+						/* check if 6 digites or more are pressed */
+						if(u8_counter >= SIX)
 						{
-							LCD_displayString("Correct Password");
-							u8_waiting_flag = FLAG_HIGH;
+							if(u8_pass_match_flag == MATCHED)
+							{
+								LCD_displayString("Correct Password");
+								u8_waiting_flag = FLAG_HIGH;
+							}
+
 						}
-
+						/* reset the matching flag and array counter */
+						u8_pass_match_flag = CLEAR;
+						u8_counter=CLEAR;
 					}
-					/* reset the matching flag and array counter */
-					u8_pass_match_flag = CLEAR;
-					u8_counter=CLEAR;
-				}
 
-				else if(au8_Key == arr_passcode[u8_counter])
-				{
-					/* check if the 6 digits entered correctly */
-					if(u8_counter==FIVE && u8_pass_match_flag==CLEAR)
+					else if(au8_Key == arr_passcode[u8_counter])
 					{
-						u8_pass_match_flag = MATCHED;
+						/* check if the 6 digits entered correctly */
+						if(u8_counter==FIVE && u8_pass_match_flag==CLEAR)
+						{
+							u8_pass_match_flag = MATCHED;
+						}
+						 /* increment the array counter */
+						u8_counter++;
 					}
-					 /* increment the array counter */
-					u8_counter++;
+
+					else
+					{
+						/* raise a the flag if any mismatch happened */
+						u8_pass_match_flag = MISMATCHED;
+
+						/* increment the array counter */
+						u8_counter++;
+					}
 				}
 
-				else
-				{
-					/* raise a the flag if any mismatch happened */
-					u8_pass_match_flag = MISMATCHED;
-
-					/* increment the array counter */
-					u8_counter++;
-				}
 			}
 
 		}
